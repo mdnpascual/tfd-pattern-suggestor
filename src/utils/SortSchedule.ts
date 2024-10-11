ad0faaa8c29e6f69c00dc1d232178b6d6e9e6c4f
mdnpascual <mdnpascual@yahoo.com.ph>
2024-10-11 12:18:40 -0600

    change sorting logic

--- src/utils/SortSchedule.ts
+++ src/utils/SortSchedule.ts
@@ -18,7 +18,7 @@ const SortSchedule = (
 		const matchedCombined = presets.filter(preset =>
 			(item.rewards.reward_type === 'Reactor' &&
 				item.rewards.reactor_element_type === preset.element && matchingLocation.secondaryElement === preset.element &&
-				item.rewards.weapon_rounds_type === (preset.ammoType + ' Rounds') && (matchingLocation.secondaryAmmo === (preset.ammoType + ' Rounds') || matchingLocation.secondaryAmmo === "*") &&
+				(item.rewards.weapon_rounds_type === (preset.ammoType + ' Rounds') || matchingLocation.secondaryAmmo === "*") && (matchingLocation.secondaryAmmo === (preset.ammoType + ' Rounds') || matchingLocation.secondaryAmmo === "*") &&
 				item.rewards.arche_type === preset.skillType && matchingLocation.secondarySkill === preset.skillType &&
 				(isNaN(filteredDropRate) || (!isNaN(filteredDropRate) && matchingLocation.reactorPerMin + (matchingLocation.secondaryReactorPerMin === 0 ? (matchingLocation.reactorPerMin * estimateSecondaryRatio) : matchingLocation.secondaryReactorPerMin) >= filteredDropRate)))
 		);
@@ -35,7 +35,7 @@ const SortSchedule = (
 				(isNaN(filteredDropRate) || (!isNaN(filteredDropRate) && matchingLocation.reactorPerMin >= filteredDropRate)))
 		);
 
-		remainingPresets = presets.filter(preset =>
+		remainingPresets = remainingPresets.filter(preset =>
 			!matchedRotation.map(preset => preset.element).includes(preset.element)
 		);
 
@@ -65,48 +65,25 @@ const SortSchedule = (
 	}, [] as SchedulePresetObject[]);
 
 	newFilteredSchedule.sort((a, b) => {
-		const aCombinedLength = a.combined.length;
-		const bCombinedLength = b.combined.length;
+		const aTotalLength = a.combined.length + a.rotation.length + a.static.length;
+		const bTotalLength = b.combined.length + b.rotation.length + b.static.length;
 
-		// Sort by combined length first
-		if (aCombinedLength !== bCombinedLength) {
-			return bCombinedLength - aCombinedLength;
-		}
+		const aCombinedScore = locationData[a.location].reactorPerMin + (locationData[a.location].secondaryReactorPerMin === 0 ? locationData[a.location].reactorPerMin * estimateSecondaryRatio : locationData[a.location].secondaryReactorPerMin);
+		const bCombinedScore = locationData[b.location].reactorPerMin + (locationData[b.location].secondaryReactorPerMin === 0 ? locationData[b.location].reactorPerMin * estimateSecondaryRatio : locationData[b.location].secondaryReactorPerMin);
 
-		// Calculate scores for combined if lengths are equal
-		const aScore = locationData[a.location].reactorPerMin + (locationData[a.location].secondaryReactorPerMin === 0 ? locationData[a.location].reactorPerMin * estimateSecondaryRatio : locationData[a.location].secondaryReactorPerMin);
-		const bScore = locationData[b.location].reactorPerMin + (locationData[b.location].secondaryReactorPerMin === 0 ? locationData[b.location].reactorPerMin * estimateSecondaryRatio : locationData[b.location].secondaryReactorPerMin);
-		if (aScore !== bScore && aCombinedLength !== 0) {
-			return bScore - aScore;
-		}
-
-		const aRotationLength = a.rotation.length;
-		const bRotationLength = b.rotation.length;
-
-		// Sort by rotation length if scores are equal
-		if (aRotationLength !== bRotationLength) {
-			return bRotationLength - aRotationLength;
-		}
-
-		// Compare scores for rotation if lengths are equal
 		const aRotationScore = locationData[a.location].reactorPerMin;
 		const bRotationScore = locationData[b.location].reactorPerMin;
-		if (aRotationScore !== bRotationScore && aRotationLength !== 0) {
-			return bRotationScore - aRotationScore;
-		}
 
-		const aStaticLength = a.static.length;
-		const bStaticLength = b.static.length;
-
-		// Sort by static length if rotation scores are equal
-		if (aStaticLength !== bStaticLength) {
-			return bStaticLength - aStaticLength;
-		}
-
-		// Compare scores for static if lengths are equal
 		const aSecondaryScore = (locationData[a.location].secondaryReactorPerMin === 0 ? locationData[a.location].reactorPerMin * estimateSecondaryRatio : locationData[a.location].secondaryReactorPerMin);
 		const bSecondaryScore = (locationData[b.location].secondaryReactorPerMin === 0 ? locationData[b.location].reactorPerMin * estimateSecondaryRatio : locationData[b.location].secondaryReactorPerMin);
-		return bSecondaryScore - aSecondaryScore;
+
+		if (aTotalLength !== bTotalLength) {
+			const aTotalScore = (a.combined.length * aCombinedScore) + (a.rotation.length * aRotationScore) + (a.static.length * aSecondaryScore)
+			const bTotalScore = (b.combined.length * bCombinedScore) + (b.rotation.length * bRotationScore) + (b.static.length * bSecondaryScore)
+			return bTotalScore - aTotalScore;
+		}
+
+		return bTotalLength - aTotalLength;
 	});
 
 	setFilteredSchedule(newFilteredSchedule);
